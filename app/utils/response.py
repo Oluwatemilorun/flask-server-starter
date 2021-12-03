7b8476a6314c0c2182b3ac588dd78188a226bbc2
Isaac Oluwatemilorun <akiolisa@gmail.com>
2021-12-03 10:40:28 +0100

    improved responses

--- app/utils/response.py
+++ app/utils/response.py
@@ -1,10 +1,13 @@
+from flask import jsonify
+
+
 def success(data, message="operation successful", status=200):
     """
     Formatted success response method for easy access
             :return: object, int
     """
 
-    return {"message": message, "data": data, "status": status}, status
+    return jsonify({"message": message, "payload": data, "status": "success"}), status
 
 
 def error(errors=[], message="Error occured. Try again later", status=500):
@@ -13,4 +16,4 @@ def error(errors=[], message="Error occured. Try again later", status=500):
             :return: object, int
     """
 
-    return {"message": message, "errors": errors, "status": status}, status
+    return jsonify({"message": message, "errors": errors, "status": "error"}), status
