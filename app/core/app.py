6323f862aac0aac10dec2129ce241ad5019d6fca
Isaac Oluwatemilorun <akiolisa@gmail.com>
2021-12-03 09:17:01 +0100

    implemented global error handler

--- app/core/app.py
+++ app/core/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+from marshmallow import ValidationError
 
 from app.config import config
+from app.utils.extensions import args
+from app.utils.response import error
+
+
+def register_api_error_handlers(app: Flask) -> None:
+    """
+    Register error handlers for API.
+
+    These are generic handlers that apply to all API requests.
+    """
+
+    @args.error_handler
+    def handle_error(err, req, schema, status_code, headers):  # type: ignore
+        raise ValidationError(err.messages)
+
+    @app.errorhandler(ValidationError)
+    def validation_error(err):  # type: ignore
+        return error(errors=error.normalized_messages(), status=400)
 
 
 def create_app(config_name: str) -> Flask:
@@ -10,6 +29,8 @@ def create_app(config_name: str) -> Flask:
 
     # Note: unsafe, better set origins to known hosts
     CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    register_api_error_handlers(app)
 
     return app
 
---
+++ app/utils/extensions.py
@@ -0,0 +1,3 @@
+from flask_rest_api.arguments import ArgumentsMixin
+
+args = ArgumentsMixin.ARGUMENTS_PARSER
